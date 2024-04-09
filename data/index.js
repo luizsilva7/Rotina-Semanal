26dff591d0d019e7840b6c2ee77ac970d54f052b
vicLira <victor.liracarlos@gmail.com>
2024-04-09 14:26:03 -0300

    adição do arquivo data

---
+++ data/index.js
@@ -0,0 +1,46 @@
+const DATA = [ 
+    {
+
+    id:'1',
+    porcentagem:'5.95%',
+    titulo:'Transporte Ida e Volta para a escola',
+    imagem:'5.png',
+    descricao:'Segunda a Sexta',
+
+}, {
+
+    id:'2',
+    porcentagem:'41.67%',
+    titulo:'Dormir',
+    imagem:'2.png',
+    descricao:'Todos os dias',
+
+},  {
+
+    id:'3',
+    porcentagem:'15.48%',
+    titulo:'Etec',
+    imagem:'3.png',
+    descricao:'Segunda a Sexta',
+
+} ,  {
+
+    id:'4',
+    porcentagem:'8.33%',
+    titulo:'Almoço e Janta',
+    imagem:'1.png',
+    descricao:'Todos os dias',
+
+} ,  {
+
+    id:'5',
+    porcentagem:'28.57%',
+    titulo:'Arrumar e Organizar a casa',
+    imagem:'6.png',
+    descricao:'Todos os dias',
+
+}
+
+]
+
+export default DATA;
